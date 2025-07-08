8186ec3ceaba32912f0bcda1b037d72a3d6a0e65
MatsNau <163524842+MatsNau@users.noreply.github.com>
2025-07-08 16:12:42 +0200

    fix path error

--- server/index.js
+++ server/index.js
@@ -3,6 +3,12 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import axios from 'axios';
 import crypto from 'crypto';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config();
 
