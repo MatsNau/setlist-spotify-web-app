047db1e16cf098527da8ddd498ac3eea7cce7a86
MatsNau <163524842+MatsNau@users.noreply.github.com>
2025-07-08 16:32:50 +0200

    added port

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://setlist-spotify-web-app.onrender.com:3001',
         changeOrigin: true,
       }
     }
